f90e803a64a381f9256701384ce695678335a7a5
Nicholas English <35505237+NWEenglish@users.noreply.github.com>
2018-03-26 19:25:22 -0400

    Add files via upload

---
+++ EGR107_Sec006_Team3_PA14/EGR107_Sec006_Team3_PA14.h
@@ -0,0 +1,56 @@
+/*********************************************************************
+*
+* Author: Nicholas English
+* Course: EGR-107-006
+* Date of Completion: 3/14/2018
+* Program Name: EGR107_Sec006_English_Assignment4.h
+* Inputs: None.
+* Outputs: None.
+* Description: Part one of three requirements for the assignment. This
+*	program holds the necessary information to control the motors of
+*	the robot.
+*
+*********************************************************************/
+
+/* #define speeds */
+#define NORMAL_SPEED 110
+
+/* #define times */
+#define TURN_240_NORM_TIME	// Normal 240* turn
+#define TURN_240_RVRS_TIME	// Reversed 240* turn.
+
+/* #define for right motor */
+#define RIGHT_PWM_PIN 6
+#define RIGHT_IN1_PIN 7
+#define RIGHT_IN2_PIN 8
+
+/* #define for left motor */
+#define LEFT_PWM_PIN 11
+#define LEFT_IN1_PIN 9
+#define LEFT_IN2_PIN 10
+
+/* All information needed for motors */
+struct robotMotors {
+	int leftPWM;
+	int rightPWM;
+};
+
+/* All states for the robot */
+enum states {
+	START,
+	FORWARD,
+	TURN_240_NORM_STATE,
+	TURN_240_RVRS_STATE,
+  TURN_180_STATE,
+	END
+};
+
+/* Structure of the robot */
+struct Robot {
+	struct robotMotors motors;
+	enum states currentState = START;
+};
+
+
+
+
---
+++ EGR107_Sec006_Team3_PA14/EGR107_Sec006_Team3_PA14.ino
@@ -0,0 +1,51 @@
+/*********************************************************************
+* Author: Team 3
+* Course: EGR-107-006
+* Date of Completion: IN_PROGRESS
+* Program Name: EGR107_Sec006_Team3_PA14.h
+* Inputs: None.
+* Outputs: None.
+* Description: The main file for running the robot in the final
+*   competition.
+*********************************************************************/
+
+/* Function Declaration(s) */
+void stopMotors();
+void moveForward();
+void reverseMotors();
+void turnRight();
+void turnLeft();
+
+/* Global Variable(s) */
+int LMSpeed = 200;
+int RMSpeed = 200;
+int rightPWM = 6;
+int right1 = 7;
+int right2 = 8;
+int left1 = 9;
+int left2 = 10;
+int leftPWM = 11;
+
+void setup() {
+
+  /* Initialize serial communication */
+  Serial.begin(9600);
+
+  // Setup for right motor
+  pinMode(rightPWM, OUTPUT);
+  pinMode(right1, OUTPUT);
+  pinMode(right2, OUTPUT);
+
+  // Setup for left motor
+  pinMode(left1, OUTPUT);
+  pinMode(left2, OUTPUT);
+  pinMode(leftPWM, OUTPUT);
+
+  // Delay before movement
+  delay(2000);
+
+}
+
+void loop() {
+
+}
